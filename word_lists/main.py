70ec885120b21764068b5a005de19e4874987ec5
OzeelCharles <charles.ozeel@gmail.com>
2026-04-25 17:45:30 +0200

    final change of the main file with add some text around the return

--- word_lists/main.py
+++ word_lists/main.py
@@ -2,15 +2,20 @@ from code_function import occ_word_file_and_reversable
 
 
 def main():
-    rank = None  # add a value if you want
+    rank = 10  # add a value if you want be carefull about lenght of each list
 
     counter_occ, is_reversable = occ_word_file_and_reversable(
         "top_freq_5000_hu.txt",
         rank
     )
-
-    print(counter_occ[:10])  # change the value if you want
-    print(is_reversable)
+    print('-'*len(str(counter_occ[:rank])))
+    print(f"Top {rank} most frequent words:")
+    print(counter_occ[:rank])
+    print('-'*len(str(counter_occ[:rank])))
+    print(f"words among the top {rank} of frequence")
+    print("whose reverse also exists in the top 5000:")
+    print(is_reversable[:rank])
+    print('-'*len(str(counter_occ[:rank])))
 
 
 if __name__ == "__main__":
